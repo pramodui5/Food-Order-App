067428253fc28bd4aaab206000be9b265942ffc5
Pramod Bhoi <mail.pramod.bhoi@gmail.com>
2022-01-22 23:21:56 +0530

    header meals meal item meal summary

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import {Fragment} from 'react';
+
+import classes from './Header.module.css';
+import bannerImage from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+    return (
+        <Fragment>        
+            <header className={classes.header}>
+                <a href="#" className={classes.logo}>ReactMeals</a>
+                <HeaderCartButton />
+            </header>
+            <div>
+                <img src={bannerImage} className={classes['main-banner']} alt="Delicious food" />
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header;
